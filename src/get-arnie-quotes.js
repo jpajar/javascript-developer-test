e93b04ff4b150d87e77509a86b3c6f307018437d
Jasper Pajar <jPajar@oes.edu.au>
2021-10-13 11:19:29 +1100

    finished getArnieQuotes fn

--- src/get-arnie-quotes.js
+++ src/get-arnie-quotes.js
@@ -1,10 +1,26 @@
-const { httpGet } = require('./mock-http-interface');
+const { httpGet } = require("./mock-http-interface");
 
-const getArnieQuotes = async (urls) => {
+const getArnieQuotes = async urls => {
   // TODO: Implement this function.
-  // return results;
+  // for (url of urls) {
+  //   results.push(await getResult(url));
+  // }
+  const results = await Promise.all(
+    urls.map(async function(url) {
+      try {
+        return await getResult(url);
+      } catch (err) {
+        throw err;
+      }
+    })
+  );
+  return results;
 };
-
+const getResult = async url => {
+  const res = await httpGet(url);
+  const { message } = JSON.parse(res.body);
+  return res.status == 200 ? { "Arnie Quote": message } : { FAILURE: message };
+};
 module.exports = {
-  getArnieQuotes,
+  getArnieQuotes
 };
